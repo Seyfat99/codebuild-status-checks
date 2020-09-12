c27dd68703ff0c36c0f8c0942ffe71edb412ba34
Seyfat Khamidov <seyfat123@gmail.com>
2020-09-12 16:15:44 -0400

    initial

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { InfraStack } from '../lib/infra-stack';
+import { GitHubStatusChecks } from '../lib/codebuild';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack');
+new GitHubStatusChecks(app, 'GitHubStatusChecks');
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,9 +1,24 @@
-import * as cdk from '@aws-cdk/core';
+// import * as cdk from '@aws-cdk/core';
+// import * as codebuild from '@aws-cdk/aws-codebuild';
 
-export class InfraStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+// import { Construct, SecretValue, Stack, StackProps, Fn } from "@aws-cdk/core";
 
-    // The code that defines your stack goes here
-  }
-}
+// export class InfraStack extends cdk.Stack {
+//   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+
+//     const gitHubSource = codebuild.Source.gitHub({
+//       owner: 'Seyfat99',
+//       repo: 'codebuild-status-checks',
+//       webhook: true, // optional, default: true if `webhookFilters` were provided, false otherwise
+//       webhookFilters: [
+//           codebuild.FilterGroup.inEventOf(codebuild.EventAction.PULL_REQUEST_CREATED),
+//           codebuild.FilterGroup.inEventOf(codebuild.EventAction.PULL_REQUEST_UPDATED)
+//       ],
+//   });
+
+//   const porject = new codebuild.Project(this, 'GithubStatusChecks', {
+//       source: gitHubSource,
+//   });
+//   }
+// }
